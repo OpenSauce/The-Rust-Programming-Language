0b215bc9a0f10fd5eacd38c9e23fe30e28c46a1b
OpenSauce <lawleycobhc@gmail.com>
2022-04-24 14:30:22 +0100

    Started working on the example project

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -5,17 +5,26 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
 
-    println!("Rect1: {}, Area: {}", rect1, area(&rect1))
-}
+    let rect2 = Rectangle { width: 20, ..rect1 };
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.height * rectangle.width
+    println!("Rect1: {}, Area: {}", rect1, rect1.area());
+    println!("Can rect1 hold rect2: {}", rect1.can_hold(&rect2));
 }
 
 impl fmt::Display for Rectangle {
